689c4338da9c14abf9776be71b70d160c33bcbbf
Keerthi <keerthidarapureddi@gmail.com>
2018-10-06 12:05:59 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

---
+++ 4.2 Evaluate Postfix/Solution.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+import java.util.Stack;
+class Postfixeval 
+{
+	public void Postfixeval(String s) 
+	    {  
+			String str[] = s.split(" ");
+			int n = 0, l = str.length;
+			Stack<Integer> stack = new Stack<>();
+			for (int i = 0; i < l; i++) 
+			{
+				try 
+				{
+					n = Integer.parseInt(str[i]);
+					stack.push(n);
+				} 
+				catch (NumberFormatException e) 
+				{
+					int v1 = stack.pop();
+					int v2 = stack.pop();
+					switch (str[i].charAt(0)) 
+					{
+						case '+':
+							n = v1 + v2;
+							break;
+						case '-':
+							n = v2 - v1;
+							break;
+						case '*':
+							n = v1 * v2;
+							break;
+						case '/':
+							n = v2 / v1;
+							break;
+
+					}
+					stack.push(n);
+				}
+			}
+		System.out.println(stack.pop());
+	}
+}
+public class Solution
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter String");
+		String s=sc.nextLine();		
+		Postfixeval p=new Postfixeval();
+		p.Postfixeval(s);
+	 } 
+
+}
+
