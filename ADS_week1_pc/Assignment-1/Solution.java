ecf1c13f1a1f703d84f9df6ed6842a6b03aec144
Keerthi <keerthidarapureddi@gmail.com>
2018-10-08 22:47:22 +0530

    "check_style_score -> 6 of 12 passed. style score: 0/1 "

--- ADS_week1_pc/Assignment-1/Solution.java
+++ ADS_week1_pc/Assignment-1/Solution.java
@@ -1,42 +1,48 @@
+
+
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Scanner;
 import java.util.Stack;
-class AddLargeNumbers 
-{
-    public static LinkedList numberToDigits(String number) 
-    {
-    	LinkedList<String> lt=new LinkedList<>();
-    	lt.add(number);
-        Iterator<String> itr=lt.iterator();
-        while(itr.hasNext()) {
-        	System.out.println(itr.next());
-        }
-        AddLargeNumbers.digitsToNumber(lt);
-        	return lt;
+
+class AddLargeNumbers {
+    
+    public static LinkedList numberToDigits(String number) {
+    	
+	LinkedList<String> lt=new LinkedList<>();
+	lt.add(number);
+    Iterator<String> itr=lt.iterator();
+    while(itr.hasNext()) {
+    	System.out.println(itr.next());
+    }
+    AddLargeNumbers.digitsToNumber(lt);
+    	return lt;
 
     }
-    public static String digitsToNumber(LinkedList list) 
-    {
-		return null;
+
+    public static String digitsToNumber(LinkedList list) {
+    	String s="";
+    	for(int i=0;i<list.size();i++) {
+    		 s=(String) list.removeLast();
+    	}
+		return s;
 
     }
-   public static LinkedList addLargeNumbers(LinkedList list1, LinkedList list2)
-    {
-	   LinkedList<Stack> list3=new LinkedList<>();
-  	 list3.addAll(list1);
-  	 list3.addAll(list2);
-  	 return list3;	
+
+    public static LinkedList addLargeNumbers(LinkedList list1, LinkedList list2) {
+    	LinkedList<Stack> list3=new LinkedList<>();
+    	 list3.addAll(list1);
+    	 list3.addAll(list2);
+    	 return list3;
     }
 }
-public class Solution 
-{
-    public static void main(String[] args) 
-    {
-    	Scanner sc=new Scanner(System.in);
-    	String input = sc.nextLine();
-        String p = sc.nextLine();
-        String q = sc.nextLine();
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        String p = sc.next();
+        String q = sc.next();
         switch(input){
             case "numberToDigits":
                 LinkedList pDigits = AddLargeNumbers.numberToDigits(p);
@@ -52,5 +58,7 @@ public class Solution
                 System.out.println(AddLargeNumbers.digitsToNumber(result));
                 break;
         }
-    }    
+    }
+    
 }
+
